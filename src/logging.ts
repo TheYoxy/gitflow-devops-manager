f0df0aae24ba1bf8b1792c51bfb56e99830ab5f6
Floryan Simar <floryansimar@gmail.com>
2019-09-10 16:57:09 +0200

    test(release): Added default logger to console for tests

--- src/logging.ts
+++ src/logging.ts
@@ -1,46 +1,50 @@
 import * as winston from 'winston';
 
 const logFile = new winston.transports.File(
-    {
-        dirname: 'logs',
-        filename: 'out.log',
-        level: 'debug',
-        maxsize: 5242880, // 5MB
-        maxFiles: 5,
-        format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] }),
-            winston.format.printf(
-                ({ level, message, timestamp, label, metadata }) => {
-                  let s = `[${timestamp}] ${level}`;
-                    if (label) {
-                      s += ` {${label}}`;
-                    }
-                  s += `: ${message}`;
-                    if (metadata) {
-                        s += ` ${JSON.stringify(metadata)}`;
-                    }
-                    return s;
-                })
-        ),
-    });
+  {
+    dirname: 'logs',
+    filename: 'out.log',
+    level: 'debug',
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.metadata({fillExcept: ['message', 'level', 'timestamp', 'label']}),
+      winston.format.printf(
+        ({level, message, timestamp, label, metadata}) => {
+          let s = `[${timestamp}] ${level}`;
+          if (label) {
+            s += ` {${label}}`;
+          }
+          s += `: ${message}`;
+          if (metadata) {
+            s += ` ${JSON.stringify(metadata)}`;
+          }
+          return s;
+        })
+    ),
+  });
 
 const jsonLogFile = new winston.transports.File(
-    {
-        dirname: 'logs',
-        filename: 'out.json.log',
-        level: 'debug',
-        maxsize: 5242880, // 5MB
-        maxFiles: 5,
-        format: winston.format.combine(
-            winston.format.timestamp(),
-            winston.format.metadata(),
-            winston.format.prettyPrint({ depth: 10, colorize: false })
-        ),
-    });
+  {
+    dirname: 'logs',
+    filename: 'out.json.log',
+    level: 'debug',
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.metadata(),
+      winston.format.prettyPrint({depth: 10, colorize: false})
+    ),
+  });
 
 export const logging = winston.createLogger(
-    {
-        transports: [logFile, jsonLogFile],
-        exceptionHandlers: [logFile],
-    });
+  {
+    transports: [logFile, jsonLogFile],
+    exceptionHandlers: [logFile],
+  });
+
+if (process.env.NODE_ENV === 'test') {
+  logging.add(new winston.transports.Console({format: winston.format.simple(),}));
+}
